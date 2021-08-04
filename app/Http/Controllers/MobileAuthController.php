04371de5733b31f26a1d32934094e6074b55f8a5
Mike Dougllas <mdougllas@gmail.com>
2021-08-03 20:15:37 -0400

    mobile auth

---
+++ app/Http/Controllers/MobileAuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class MobileAuthController extends Controller
+{
+    /**
+     * Verify user info and issue new token
+     * Destroy old token if found
+     *
+     * @param  mixed $request
+     * @return string
+     */
+    public function requestToken(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $user->tokens()->where('name', $request['device_name'])->delete();
+
+        return $user->createToken($request->device_name)->plainTextToken;
+    }
+
+    public function destroyToken(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MobileAuthController;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('mobile/register', [RegisteredUserController::class, 'store']);
+
+Route::post('mobile/login', [MobileAuthController::class, 'requestToken']);
+
+Route::middleware('auth:sanctum')->post('mobile/logout', [MobileAuthController::class, 'destroyToken']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
