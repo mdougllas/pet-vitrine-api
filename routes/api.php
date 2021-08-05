9b227ed80b34aecfe737228ce057c6258d730183
Mike Dougllas <mdougllas@gmail.com>
2021-08-04 21:36:30 -0400

    goot testing progress

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use FacebookAds\Api;
 use Illuminate\Http\Request;
+use FacebookAds\Object\AdAccount;
+use FacebookAds\Logger\CurlLogger;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MobileAuthController;
 use Laravel\Fortify\Http\Controllers\RegisteredUserController;
@@ -24,4 +27,34 @@ Route::middleware('auth:sanctum')->post('mobile/logout', [MobileAuthController::
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/facebook-test', function () {
+    $app_id = config('services.facebook.appId');
+    $app_secret = config('services.facebook.appSecret');
+    $access_token = config('services.facebook.accessToken');
+    $account_id = config('services.facebook.accountId');
+
+    $api = Api::init($app_id, $app_secret, $access_token);
+    $api->setLogger(new CurlLogger());
+
+    $account = new AdAccount($account_id);
+    $fields = array(
+        'impressions',
+        'ad_name'
+    );
+    $params = array(
+        'date_preset' => 'maximum',
+        // 'time_range' => array('since' => '2018-12-01', 'until' => '2021-08-01'),
+        'level' => 'ad',
+    );
+    $cursor = $account->getInsights($fields, $params)->getResponse()->getContent();
+
+    return $cursor;
+
+
+    echo json_encode((new AdAccount($account_id))->getInsights(
+        $fields,
+        $params
+    )->getResponse()->getContent(), JSON_PRETTY_PRINT);
 });
