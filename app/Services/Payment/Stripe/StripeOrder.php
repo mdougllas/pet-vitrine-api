f05ff15568d6befbbd245fc78cfe2874fcdd4b5b
Mike Dougllas <mdougllas@gmail.com>
2022-06-05 10:29:49 -0400

    refactored stripe payment validation

--- app/Services/Payment/Stripe/StripeOrder.php
+++ app/Services/Payment/Stripe/StripeOrder.php
@@ -2,10 +2,13 @@
 
 namespace App\Services\Payment\Stripe;
 
+use Illuminate\Support\Str;
+use App\Exceptions\HttpException;
+use App\Exceptions\PaymentException;
 use App\Helpers\HandleHttpException;
-use App\Services\Payment\PaymentInterface;
 use Illuminate\Support\Facades\Http;
 use App\Services\Payment\Stripe\Stripe;
+use App\Services\Payment\PaymentInterface;
 
 class StripeOrder extends Stripe implements PaymentInterface
 {
@@ -40,12 +43,50 @@ class StripeOrder extends Stripe implements PaymentInterface
      *
      * @return void;
      */
-    public function validatePaymentId($id)
+    public function validatePayment($id, $amount)
     {
         $paymentIntent = Http::asForm()
             ->withBasicAuth($this->secretKey, '')
             ->post("$this->baseUrl/v1/payment_intents/$id");
-
         $paymentIntent->onError(fn ($err) => HandleHttpException::throw($err));
+
+        $intentObject = $paymentIntent->object();
+        $this->validatePaymentIsLive($intentObject);
+        $this->validateStatusSucceeded($intentObject);
+        $this->validateAmountPaid($intentObject, $amount);
+        $this->validatePaymentNotCanceled($intentObject);
+        $this->validatePaymentNotRefunded($intentObject);
+    }
+
+    private function validatePaymentIsLive($intentObject)
+    {
+        return $intentObject->livemode
+            ?? throw new PaymentException('This payment is not a live payment.', 401);
+    }
+
+    private function validateStatusSucceeded($intentObject)
+    {
+        return $intentObject->status === 'succeeded'
+            ?? throw new PaymentException('This payment was not finalized.', 401);
+    }
+
+    private function validateAmountPaid($intentObject, $amount)
+    {
+        $amountReceived = $intentObject->amount_received / 100;
+
+        return $amountReceived === $amount
+            ?? throw new PaymentException('The amount requested is different from the paid amount', 401);
+    }
+
+    private function validatePaymentNotCanceled($intentObject)
+    {
+        return !$intentObject->canceled_at
+            ?? throw new PaymentException('This payment was canceled.', 401);
+    }
+
+    private function validatePaymentNotRefunded($intentObject)
+    {
+        return !isset($intentObject->amount_refunded)
+            ?? throw new PaymentException('This payment was refunded.', 401);
     }
 }
