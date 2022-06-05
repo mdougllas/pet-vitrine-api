6dff9b50121f1adceec5c14ea8fcc8ddd642ca26
Mike Dougllas <mdougllas@gmail.com>
2022-06-05 16:24:16 -0400

    tests

--- app/Http/Controllers/FacebookAdController.php
+++ app/Http/Controllers/FacebookAdController.php
@@ -85,7 +85,7 @@ class FacebookAdController extends Controller
 
         $this->verifyAdExists($paymentId);
         $this->validatePayment($paypal, $stripe, $paymentId, $paymentProvider, $budget);
-
+        var_dump('I got here');
         $lastCampaignId = $campaign->getLastCampaign()->id;
         $adSet = $adSet->createAdSet($petName, $lastCampaignId, $zipCode, $budget);
         $adCreative = $creative->createAdCreative($url, $link, $petName);
