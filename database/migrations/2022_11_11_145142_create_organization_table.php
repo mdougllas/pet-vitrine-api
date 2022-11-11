35090e0cdf6acf28bc887a6113a34b39b23d865a
Mike Dougllas <mdougllas@gmail.com>
2022-11-11 10:18:37 -0500

    created organizations table

---
+++ database/migrations/2022_11_11_145142_create_organization_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->string('address_1');
+            $table->string('address_2');
+            $table->string('city');
+            $table->tinyInteger('latitude');
+            $table->tinyInteger('longitude');
+            $table->string('name');
+            $table->smallInteger('postal_code');
+            $table->bigInteger('petfinder_id');
+            $table->tinyText('state', 30);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organization');
+    }
+};
