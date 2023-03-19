f239a96410d2e487dc7631b663bdc8173d65dc1d
Mike Dougllas <mdougllas@gmail.com>
2023-03-19 17:16:37 -0400

    renamed ads to generate migrations

--- database/migrations/2021_08_01_165157_create_ads_table.php
+++ database/migrations/2021_08_01_165157_create_ads_table.php
@@ -16,6 +16,7 @@ class CreateAdsTable extends Migration
         Schema::create('ads', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
+
             $table->bigInteger('ad_id')->unique();
             $table->bigInteger('ad_set_id');
             $table->float('amount_spent', 8, 2)->nullable();
@@ -26,7 +27,11 @@ class CreateAdsTable extends Migration
             $table->timestamp('end_time');
             $table->integer('impressions')->nullable();
             $table->string('payment_id')->unique()->nullable();
-            $table->bigInteger('pet_id');
+            $table
+                ->foreignId('pet_id')
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
             $table->integer('results')->nullable();
             $table->integer('reach')->nullable();
             $table->timestamp('start_time');
