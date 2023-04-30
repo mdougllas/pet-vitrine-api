8c1938cc2f1355d71aa517232bf6f1f2d5c4a178
Mike Dougllas <mdougllas@gmail.com>
2023-04-30 15:06:36 -0400

    back to 30 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,8 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(StartSpiderCommand::class)
-            ->everyMinute()
-            // ->everyThirtyMinutes()
+            ->everyThirtyMinutes()
             ->sendOutputTo($this->getSpiderLogFilePath())
             ->withoutOverlapping();
 
