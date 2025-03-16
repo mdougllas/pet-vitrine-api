f9ca38784819ce6f59d6081e8042bacf8798817a
Michael Myrick <mdougllas@gmail.com>
2025-03-16 03:55:06 -0400

    refactored

--- app/Services/Spider/SpiderJobsManager.php
+++ app/Services/Spider/SpiderJobsManager.php
@@ -2,7 +2,6 @@
 
 namespace App\Services\Spider;
 
-use App\Models\Organization;
 use App\Models\SpiderJob;
 use App\Traits\Spider\UseSetOutput;
 
@@ -26,11 +25,6 @@ class SpiderJobsManager
     private $pets = null;
 
     /**
-     * @property integer $cicle
-     */
-    private $cicle = 1;
-
-    /**
      * Blueprint for SpiderJobsManager.
      *
      * @param \App\Services\Spider\HttpRequest $spider
@@ -52,101 +46,24 @@ class SpiderJobsManager
     {
         if ($this->getJobRunning()) {
             $this->output->warn("Spider is already running.");
+
             return 0;
         }
 
         $this->output->info("Spider jobs initiated.");
+
+        $this->setJobRunning(1);
+        $this->spider->setOutput($this->output);
         $this->shelters->setOutput($this->output);
         $this->pets->setOutput($this->output);
 
-        $organizations = $this->spider->getOrganizations();
-
-        $this->parseOrganizations($organizations);
-
-        $lastOrganizationId = $this->getLatestParsedPage();
-        $organizationId = $lastOrganizationId + 1;
-        $organization = Organization::find($lastOrganizationId + 1);
-
-        if (empty($organization)) {
-            $this->setLatestParsedPage($lastOrganizationId + 1);
-
-            return 0;
-        }
-
-        $pets = $this->spider->getPetsByOrganization($organization->petfinder_id);
-
-        if (!$pets) {
-            $this->output->warn("No pets received from this request. Skipping parsing pets.");
-
-            return 0;
-        }
-
-        $this->parsePetsInfo($pets, $organization->petfinder_id);
-
-        if ($organizationId >= $organizations->pagination->total_count) {
-            $this->setLatestParsedPage(0);
-
-            return 0;
-        }
-
-        $this->setLatestParsedPage($organizationId);
-        $this->setJobRunning(false);
+        $this->shelters->parseShelters();
+        $this->pets->parsePets();
 
         $this->output->info("Spider jobs finished.");
+        $this->setJobRunning(0);
 
         return 0;
-    }
-
-    /**
-     * Parse meta-data to collect number of pages.
-     *
-     * @param  Collection $result
-     * @return mixed int
-     */
-    private function parseOrganizations($result)
-    {
-        $pagination = collect($result->get('pagination'));
-        $totalShelters = $pagination->get('total_count');
-        $totalPages = $pagination->get('total_pages');
-
-        $pages = collect()->range(1, $totalPages);
-
-        $pages->each(function ($page) {
-            $this->shelters->parseShelters($page);
-        });
-
-        $this->setNumberOfShelters($totalShelters);
-
-        return;
-    }
-
-    /**
-     * Parse meta-data to collect number of pages.
-     *
-     * @param  json  $result
-     * @return mixed int
-     */
-    private function parsePetsInfo($pets, $organizationId)
-    {
-        $fromPage = 1;
-        $toPage = $pets->result->pagination->total_pages;
-
-        $pages = collect()
-            ->range($fromPage, $toPage);
-
-        $pages->each(function ($page) use ($toPage, $organizationId) {
-            $this->output->info("Parsing Page $page");
-            $this->output->info("Cicle $this->cicle");
-
-            $this->pets->parsePets($page, $organizationId);
-            $this->cicle += 1;
-
-            if ($page >= $toPage) {
-                return false;
-            }
-        });
-
-        return;
     }
 
     /**
@@ -170,55 +87,5 @@ class SpiderJobsManager
         $spiderJob->job_running = $state;
 
         $spiderJob->save();
-    }
-
-    /**
-     * Store the id for the latest parsed pet.
-     *
-     * @return Illuminate\Database\Eloquent\Collection
-     * @return Illuminate\Database\Eloquent\Collection
-     */
-    private function setLatestParsedPage($page)
-    {
-        $spiderJob = SpiderJob::first();
-        $spiderJob->last_page_processed = $page;
-
-        $spiderJob->save();
-    }
-
-    /**
-     * Retrieve the id for the latest parsed pet.
-     *
-     * @return int
-     */
-    private function getLatestParsedPage(): int
-    {
-        return SpiderJob::first()->last_page_processed;
-    }
-
-    /**
-     * Store the id for the latest parsed pet.
-     *
-     * @return Illuminate\Database\Eloquent\Collection
-     * @return Illuminate\Database\Eloquent\Collection
-     */
-    private function setNumberOfShelters($qty)
-    {
-        dd($qty);
-        $spiderJob = SpiderJob::first();
-        $spiderJob->number_of_shelters = $qty;
-
-        $spiderJob->save();
-    }
-
-    /**
-     * Retrieve the id for the latest parsed pet.
-     *
-     * @return Illuminate\Database\Eloquent\Collection
-     * @return Illuminate\Database\Eloquent\Collection
-     */
-    private function getNumberOfShelters()
-    {
-        return SpiderJob::first()->number_of_shelters;
     }
 }
