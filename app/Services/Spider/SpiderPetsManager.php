30028704b475296464b296b4d479f7ddbf26b2be
Michael Myrick <mdougllas@gmail.com>
2025-03-16 03:55:21 -0400

    refactored

--- app/Services/Spider/SpiderPetsManager.php
+++ app/Services/Spider/SpiderPetsManager.php
@@ -2,10 +2,12 @@
 
 namespace App\Services\Spider;
 
-use App\Models\Pet;
-use Illuminate\Support\Str;
 use App\Models\Organization;
+use App\Models\Pet;
+use App\Models\SpiderJob;
 use App\Traits\Spider\UseSetOutput;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class SpiderPetsManager
 {
@@ -43,62 +45,85 @@ class SpiderPetsManager
     /**
      * Start the jobs to scrape and store data.
      *
-     * @return Illuminate\Database\Eloquent\Collection;
-     * @return Illuminate\Database\Eloquent\Collection;
+     * @return void;
      */
-    public function parsePets($page, $id)
+    public function parsePets(): void
     {
-        $response = $this->spider->getPetsByOrganization($id, $page);
+        $pets = $this->spider->getPets();
+        $petCount = $this->getNumberOfPets();
 
-        if (!$response || !$response->result) {
-            $this->output->warn("No pets received from this request. Skipping parsing pets.");
+        $pagination = collect($pets->get('pagination'));
+        $totalPets = $pagination->get('total_count');
+        $totalPages = $pagination->get('total_pages');
+
+        if ($petCount === $totalPets) {
+            $this->output->info("No new pets registered.");
+
+            return;
+        }
+
+        $page = 0;
+
+        while ($totalPages > 0) {
+            $totalPages--;
+            $page++;
+
+            $this->collectPets($page);
+        }
+
+        $this->setNumberOfPets($totalPets);
+    }
+
+    private function collectPets(int $page)
+    {
+        $this->output->info("Parsing page # $page.");
+
+        $response = $this->spider->getPets($page);
+        $pets = collect($response->get('animals'));
+
+        $pets->each(fn ($pet) => $this->persistPet(collect($pet)));
+    }
+
+    private function persistPet(Collection $pet): bool
+    {
+        $name = $pet->get('name');
+        $id = $pet->get('id');
+        $species = $pet->get('species');
+
+        $this->output->info("Analizing pet $name");
+
+        if ($this->petExists($id)) {
+            $this->output->warn("Pet $id already on DB. Skipping saving the pet.");
 
             return true;
         }
 
-        $pets = collect($response->result->animals);
+        if ($this->checkDuplicateByName($pet) !== false) {
+            $this->output->warn("Pet $name is a dulicate. Skipping saving the pet.");
 
-        $pets->each(function ($pet) {
-            $petData = $pet->animal;
+            return true;
+        }
 
-            $this->output->info("This is pets loop # $this->loop");
+        if ($this->checkNameCharacters($name) > 80) {
+            $this->output->warn("The name of pet $name is too long. Skipping saving the pet.");
 
-            $this->loop += 1;
+            return true;
+        }
 
-            if ($this->petExists($petData->id)) {
-                $this->output->warn("Pet $petData->id already on DB. Skipping saving the pet.");
+        if (!$this->filterSpecies($species)) {
+            $this->output->warn("Pet $species is not a cat or a dog. Skipping saving the pet.");
 
-                return true;
-            }
+            return true;
+        }
 
-            if ($this->checkDuplicateByName($pet) !== false) {
-                $this->output->warn("Pet $petData->id is a dulicate. Skipping saving the pet.");
+        $this->savePet($pet);
 
-                return true;
-            }
-
-            if ($this->checkNameCharacters($petData->name) > 80) {
-                $this->output->warn("The name of pet $petData->id is too long. Skipping saving the pet.");
-
-                return true;
-            }
-
-            if (!$this->filterSpecies($petData->species->name)) {
-                $this->output->warn("Pet $petData->id is not a cat or a dog. Skipping saving the pet.");
-
-                return true;
-            }
-
-            $this->savePet($pet);
-        });
-
-        return false;
+        return true;
     }
 
     /**
      * Retrieve the id for the latest parsed pet.
      *
-     * @return Illuminate\Database\Eloquent\Collection
      * @return Illuminate\Database\Eloquent\Collection
      */
     private function petExists($id)
@@ -110,15 +135,13 @@ class SpiderPetsManager
      * Retrieve the id for the latest parsed pet.
      *
      * @return Illuminate\Database\Eloquent\Collection
-     * @return Illuminate\Database\Eloquent\Collection
      */
     private function checkDuplicateByName($pet)
     {
-        $petData = $pet->animal;
-        $nameMatches = Pet::where('name', $petData->name)->get();
+        $nameMatches = Pet::where('name', $pet['name'])->get();
 
         if (!$nameMatches->isEmpty()) {
-            $duplicate = $this->doubleCheckDuplicate($petData, $nameMatches);
+            $duplicate = $this->doubleCheckDuplicate($pet, $nameMatches);
 
             return $duplicate;
         }
@@ -130,14 +153,13 @@ class SpiderPetsManager
      * Retrieve the id for the latest parsed pet.
      *
      * @return Illuminate\Database\Eloquent\Collection
-     * @return Illuminate\Database\Eloquent\Collection
      */
-    private function doubleCheckDuplicate($petData, $nameMatches)
+    private function doubleCheckDuplicate($newPet, $nameMatches)
     {
-        $checkDuplicate = $nameMatches->map(function ($pet) use ($petData) {
-            $sexMatches = $pet->sex == $petData->sex;
-            $speciesMatches = $pet->species == $petData->species->name;
-            $breedMatches = $pet->breed == $petData->primary_breed->name;
+        $checkDuplicate = $nameMatches->map(function ($pet) use ($newPet) {
+            $sexMatches = $pet->sex == $newPet['gender'];
+            $speciesMatches = $pet->species == $newPet['species'];
+            $breedMatches = $pet->breed == $newPet['breeds']['primary'];
 
             return $sexMatches && $speciesMatches && $breedMatches;
         });
@@ -155,7 +177,6 @@ class SpiderPetsManager
      * Retrieve the id for the latest parsed pet.
      *
      * @return Illuminate\Database\Eloquent\Collection
-     * @return Illuminate\Database\Eloquent\Collection
      */
     private function filterSpecies($species)
     {
@@ -165,7 +186,6 @@ class SpiderPetsManager
     /**
      * Retrieve the id for the latest parsed pet.
      *
-     * @return Illuminate\Database\Eloquent\Collection
      * @return Illuminate\Database\Eloquent\Collection
      */
     private function attachToOrganization($pet, $organizationId)
@@ -186,7 +206,6 @@ class SpiderPetsManager
      * Retrieve the id for the latest parsed pet.
      *
      * @return Illuminate\Database\Eloquent\Collection
-     * @return Illuminate\Database\Eloquent\Collection
      */
     private function savePet($pet)
     {
@@ -204,5 +223,28 @@ class SpiderPetsManager
 
         $petData->save();
         $this->output->info("Pet $petId saved on database.");
+    }
+
+    /**
+     * Store the id for the latest parsed pet.
+     *
+     * @return void
+     */
+    private function setNumberOfPets($qty): void
+    {
+        $spiderJob = SpiderJob::first();
+        $spiderJob->number_of_pets = $qty;
+
+        $spiderJob->save();
+    }
+
+    /**
+     * Retrieve the id for the latest parsed pet.
+     *
+     * @return int
+     */
+    private function getNumberOfPets(): int
+    {
+        return SpiderJob::first()->number_of_pets;
     }
 }
