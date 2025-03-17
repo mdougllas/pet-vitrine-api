0e220bf5412222cf50c49d0dc71252cb74eb8ba4
Michael Myrick <mdougllas@gmail.com>
2025-03-17 11:08:23 -0400

    daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command(StartSpiderCommand::class)
-            ->everyMinute()
+            ->dailyAt('23:00')
             ->sendOutputTo($this->getSpiderLogFilePath())
             ->withoutOverlapping();
 
